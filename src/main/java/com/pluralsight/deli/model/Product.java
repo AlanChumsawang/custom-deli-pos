bc6176cd9a9433f31fcdcb554552c3d61d823129
AlanChumsawang <alan.chumsawang@outlook.com>
2024-11-08 12:13:44 -0500

    Defined Product, developed constructor, getters, and abstract methods.

--- src/main/java/com/pluralsight/deli/model/Product.java
+++ src/main/java/com/pluralsight/deli/model/Product.java
@@ -1,4 +1,21 @@
 package com.pluralsight.deli.model;
 
 public abstract class Product {
+    protected String name;
+    protected double startingPrice;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.startingPrice = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return startingPrice;
+    }
+
+    public abstract double calculateProductTotal();
 }
