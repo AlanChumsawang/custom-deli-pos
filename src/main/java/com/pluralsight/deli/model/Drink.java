8165d6137d9b6974656a0bf6f3788537bc140cb2
AlanChumsawang <alan.chumsawang@outlook.com>
2024-11-08 16:36:47 -0500

    Defined Drink & Overrided calculateProductTotal

--- src/main/java/com/pluralsight/deli/model/Drink.java
+++ src/main/java/com/pluralsight/deli/model/Drink.java
@@ -1,6 +1,25 @@
 package com.pluralsight.deli.model;
 
-public class Drink extends Product {
+import com.pluralsight.deli.model.enums.DrinkSize;
 
+public class Drink extends Product {
+    protected DrinkSize size;
+
+    public Drink(String name, DrinkSize size){
+        super(name, 0);
+        this.size = size;
+    }
+
+    @Override
+    public double calculateProductTotal() {
+        if (size == DrinkSize.SMALL) {
+            return 1.50;
+        } else if (size == DrinkSize.MEDIUM) {
+            return 2.00;
+        } else if (size == DrinkSize.LARGE) {
+            return 2.50;
+        }
+        return 0;
+    }
 }
 
